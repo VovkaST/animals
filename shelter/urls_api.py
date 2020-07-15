2262f0544a2d4a2a323bca1b996713bade5a6eb5
Сунцов Владимир <vovka.steel911@mail.ru>
2020-07-16 00:01:40 +0300

    Оптимизация api для ответов в json-структурированном формате

--- shelter/urls_api.py
+++ shelter/urls_api.py
@@ -6,9 +6,4 @@ from .views_api import APIAnimals
 urlpatterns = [
     path('token-auth/', views.obtain_auth_token),
     re_path(r'animals/(?P<pk>\d+)?', APIAnimals.as_view(), name='api_guests'),
-    # path('animals/', APIAnimals.as_view(), name='api_guest_create'),
-    # path('create/', GuestsCreateView.as_view(), name='api_guest_create'),
-    # path('view/<int:pk>/', GuestsDetailView.as_view(), name='api_guest_detail_view'),
-    # path('edit/<int:animal_id>/', GuestsEditView.as_view(), name='api_guest_edit'),
-    # path('delete/<int:animal_id>/', GuestsDelete.as_view(), name='api_guest_delete'),
 ]
--- shelter/views_api.py
+++ shelter/views_api.py
@@ -1,4 +1,3 @@
-from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,41 +6,65 @@ from .serializers import AnimalsViewSerializer, AnimalsEditSerializer
 
 
 class APIAnimals(APIView):
+
+    def json_response(self, message, success=True, status=None):
+        """
+        Формирование ответа в формате JSON
+
+        :param str message: Сообщение о результате обработки запроса
+        :param bool success: Булевое значение успешности запроса
+        :param int status: Код состояния HTTP
+        :return: Объект Response
+        """
+        response = {
+            'success': success,
+            'message': message,
+        }
+        return Response(response, status=status)
+
     def get(self, request, *args, **kwargs):
         """ Просмотр всех записей """
-        records = Animals.objects.all()
+        records = Animals.objects.filter(is_deleted=False)
         serializer = AnimalsViewSerializer(records, many=True)
         return Response({'animals': serializer.data})
 
     def post(self, request, *args, **kwargs):
         """ Создание записи """
         if not request.user.is_authenticated:
-            return Response({'error': 'Access denied!'}, status=401)
+            return self.json_response(message='Access denied!', success=False, status=401)
         records = request.data.get('animals')
         serializer = AnimalsEditSerializer(data=records)
         if serializer.is_valid(raise_exception=False):
-            news_created = serializer.save()
-            return Response({'success': 'Record with id={} was successfully created'.format(news_created)})
+            created = serializer.save()
+            return self.json_response(message='Record with id={} was successfully created'.format(created.id))
         else:
-            return Response({'error': serializer.errors})
+            return self.json_response(message=serializer.errors, success=False)
 
     def put(self, request, pk, *args, **kwargs):
         """ Изменение записи по id """
         if not request.user.is_authenticated:
-            return Response({'error': 'Access denied!'}, status=401)
-        record = get_object_or_404(Animals.objects.all(), pk=pk)
-        data = request.data.get('animals')
-        serializer = AnimalsEditSerializer(instance=record, data=data, partial=True)
-        if serializer.is_valid():
-            news_saved = serializer.save()
-            return Response({'success': 'Record with id={} was successfully updated'.format(news_saved)})
-        else:
-            return Response({'error': serializer.errors})
+            return self.json_response(message='Access denied!', success=False, status=401)
+        try:
+            record = Animals.objects.get(id=pk, is_deleted=False)
+            data = request.data.get('animals')
+            serializer = AnimalsEditSerializer(instance=record, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return self.json_response(message='Record with id={} was successfully updated'.format(pk))
+            else:
+                return self.json_response(message=serializer.errors, success=False)
+        except Animals.DoesNotExist:
+            return self.json_response(message='Record with id={} does not exist'.format(pk), success=False)
 
     def delete(self, request, pk, *args, **kwargs):
         """ Удаление записи по id """
         if not request.user.is_authenticated:
-            return Response({'error': 'Access denied!'}, status=401)
-        record = get_object_or_404(Animals.objects.all(), pk=pk)
-        record.delete()
-        return Response({'success': 'Record with id={} was successfully deleted'.format(pk)})
+            return self.json_response(message='Access denied!', success=False, status=401)
+        elif request.user.username.lower() != 'admin':
+            return self.json_response(message='Permission denied!', success=False, status=403)
+        try:
+            animal = Animals.objects.get(id=pk, is_deleted=False)
+            animal.soft_delete()
+            return self.json_response(message='Record with id={} was successfully deleted'.format(pk))
+        except Animals.DoesNotExist:
+            return self.json_response(message='Record with id={} does not exist'.format(pk), success=False)
