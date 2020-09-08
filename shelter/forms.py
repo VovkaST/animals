2fe8999f7e784ece79230f6e80042a58a4d8b779
Сунцов Владимир <vovka.steel911@mail.ru>
2020-09-08 18:25:22 +0300

    Реализован интерфейс на Bootstrap4 и JQuery 3.5.1

--- shelter/forms.py
+++ shelter/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
+
 from .models import Animals
 
 
@@ -6,3 +7,6 @@ class AnimalsForm(ModelForm):
     class Meta:
         model = Animals
         exclude = ['shelter', 'created_at', 'modified_at', 'is_deleted', 'deleted_at']
+        widgets = {
+            'special_signs': Textarea(attrs={'rows': 3, 'cols': 50})
+        }
